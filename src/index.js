9838e407b2d22076442b0f4d79c66f87e664bd57
Sultan <sitexpressorg@gmail.com>
2021-11-16 20:04:48 +0300

    Add lodash lybrary

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import {compose, pipe} from 'lodash/fp'
+
+// const str = " Hello yo " 
+// const type = "div" 
+
+const trim = str => str.trim()
+const wrap = type => str => `<${type}>${str}</${type}>`
+const toLowerCase = str => str.toLowerCase()
+
+
+const transform = pipe(trim, toLowerCase, wrap("div"))
+console.log(input)
+
+// const messege = console.log(toLowerCase(trim(wrap(type, str))))
